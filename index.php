8aa28ec3fc6c62d4084590821e2f38f4d7d77dd9
Romain Fievez <romain.fievezjr@gmail.com>
2020-06-01 20:32:53 +0200

    Finalisation accueil

--- index.php
+++ index.php
@@ -1,65 +1,170 @@
 <html>
     <head>
-        <script type="text/javascript" src="./views/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="./views/css/bootstrap.min.css">
-        <link rel="stylesheet" href="./views/css/navbar.css">
-        <link rel="stylesheet" href="./views/css/index.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="./vue/css/materialize.css">
+        <link rel="stylesheet" href="./vue/css/index.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script type="text/javascript" src="./vue/js/materialize.js"></script>
+        <script type="text/javascript" src="./vue/js/index.js"></script>
     </head>
     <body>
-        <nav class="site-header sticky-top py-1">
-            <div class="container d-flex flex-column flex-md-row justify-content-between">
-                <a class="py-2 d-none d-md-inline-block" href="index.php">Accueil</a>
-                <a class="py-2 d-none d-md-inline-block" href="./controleur/matiere.php">Matière</a>
-                <a class="py-2 d-none d-md-inline-block" href="#">Etudiants</a>
-                <a class="py-2 d-none d-md-inline-block" href="./controleur/candidater.php">Candidater</a>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="../index.php" class="brand-logo"><img src="./vue/ressources/Logo-Dauphine-PSL.png" class="imgDauphine"></a>
+                <a class="brand-logo center">Master MIAGE</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="./controleur/matiere.php">Matière</a></li>
+                <li><a href="./controleur/etudiant.php">Etudiants</a></li>
+                <li><a href="./controleur/candidater.php">Candidater</a></li>
+                </ul>
             </div>
         </nav>
-		<div class="divGenerale">
-            <div class="presentation">
-                <div>
-                    <h1>Présentation de la formation</h1>
-                </div>
-                <div>
-                    <span>
-                        La 1ère année de Master - Méthodes Informatiques Appliquées pour la Gestion des Entreprises (MIAGE) a pour vocation de former des professionnels et des chercheurs maîtrisant parfaitement l'outil informatique et possédants une bonne connaissance de l'organisation et du fonctionnement de l'entreprise. MIAGE garde un ancrage fort dans le réseau national des MIAGE, qui regroupe l'ensemble des formations analogues dans vingt universités française.
+        <div>
+            <img src="./vue/ressources/UNIVERSITE-DAUPHINE-index.jpg" class="imgSection1">
+        </div>
+        <div class="presentation">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12">
+                        <h1>Présentation de la formation</h1>
+                    </div>
+                    <div>
+                        <p>
+                            La 1ère année de Master - Méthodes Informatiques Appliquées pour la Gestion des Entreprises (MIAGE) a pour vocation de former des professionnels et des chercheurs maîtrisant parfaitement l'outil informatique et possédants une bonne connaissance de l'organisation et du fonctionnement de l'entreprise. Notre MIAGE garde un ancrage fort dans le réseau national des MIAGE, qui regroupe l'ensemble des formations analogues dans vingt universités française.
+                        </p>
+                        <p>Les objectifs de la première année du Master sont: </p>
                         <ul>
-                            <li>Utiliser, maîtriser et intégrer les technologies informatique</li>
-                            <li>D'aborder la modélisation de systèmes d'information et les méthodes de conduites de projet</li>
-                            <li>Comprendre le fonctionnement des organisations, leurs structures, leurs impératifs stratégiques et l'ensemble des interactions, notamment dans la dimension opérationnelle et humaine</li>
-                            <li>Apprendre et maîtriser l'anglais et une LV2 obligatoire, avec une certification en anglais par le TOEIC</li>
+                            <li class="col s12 offset-s1 bulletObjectif">Utiliser, maîtriser et intégrer les technologies informatique</li>
+                            <li class="col s12 offset-s1 bulletObjectif">D'aborder la modélisation de systèmes d'information et les méthodes de conduites de projet</li>
+                            <li class="col s12 offset-s1 bulletObjectif">Comprendre le fonctionnement des organisations, leurs structures, leurs impératifs stratégiques et l'ensemble des interactions, notamment dans la dimension opérationnelle et humaine</li>
+                            <li class="col s12 offset-s1 bulletObjectif">Apprendre et maîtriser l'anglais et une LV2 obligatoire, avec une certification en anglais par le TOEIC</li>
                         </ul>
-                    </span>
+                    </div>
+                    <div class="col s12">
+                        <p>La deuxième année du Master offre une spécialisation que l'étudiant peut choisir :</p>
+                        <ul class="collapsible">
+                            <li>
+                                <div class="collapsible-header">
+                                    <i class="material-icons right">expand_more</i>
+                                    Système d'Information et Transformation Numérique (SITN)
+                                </div>
+                                <div class="collapsible-body">
+                                    <p>Le parcours Systèmes d'Information et Transformation Numérique (MIAGE) a pour objectifs de former des spécialistes en transformation numérique des systèmes d'information, et apporte une maîtrise des nouvelles technologies utilisées dans le développement des systèmes d'informations et une connaissance du fonctionnement des organisations et des méthodes de conduite de projet. Formation de haut niveau fortement axée sur le développement des systèmes d'information et le management des technologies nouvelles.</p>
+                                    <p>Les objectifs de cette spécialité sont: </p>
+                                    <ul class="offset-s1">
+                                        <li>-  Capacité de comprendre le fonctionnement d'une organisation, ses structures, ses impératifs stratégiques, des outils formels de management scientifique et des nouvelles technologies de l'information</li>
+                                        <li>-  Utilisation et maîtrise de technologies informatiques nouvelles qui permettent de faire évoluer les solutions informatiques, notamment vers les applications web</li>
+                                        <li>-  Maîtrise des compétences informatiques nécessaires pour la gestion efficace des connaissances et des gros volumes de données</li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="collapsible-header">
+                                    <i class="material-icons right">expand_more</i>
+                                    Informatique Décisionnelle (ID)
+                                </div>
+                                <div class="collapsible-body">
+                                    <p>Le parcours MIAGE Informatique Décisionnelle vise à doter les étudiants de compétences scientifiques pour améliorer le fonctionnement des organisations par l'utilisation des outils de l'informatique décisionnelle et l'application d'une approche scientifique. Il s'agit donc de former des professionnels ayant une double compétence en informatique.</p>
+                                    <p>Les objectifs de cette spécialité sont: </p>
+                                    <ul>
+                                        <li>-  Compétences approfondies en informatique et informatique décisionnelle, (bases de données, data warehouse, nouvelles technologies de l'information)</li>
+                                        <li>-  Compétences scientifiques pour comprendre les organisations et leurs impératifs stratégiques et améliorer leur fonctionnement</li>
+                                        <li>-  Capacité à modéliser et résoudre des problèmes de décision à travers la maîtrise de différents outils décisionnels</li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="collapsible-header">
+                                    <i class="material-icons right">expand_more</i>
+                                    Informatique pour la Finance (IF)
+                                </div>
+                                <div class="collapsible-body">
+                                    <p>Le parcours MIAGE-IF a pour objectif de former des informaticiens spécialistes des applications financières, capables de maîtriser tout au long de leur carrière les problématiques spécifiques à ce domaine et aptes à proposer et à mettre en œuvre les solutions adéquates. La formation est à 75% une formation dédiée aux connaissances nécessaires à l’ingénierie d’applications de haute qualité.</p>
+                                    <p>Les objectifs de cette spécialité sont: </p>
+                                    <ul>
+                                        <li>-  Connaissance approfondie des technologies de pointe pour la conception de système d'information</li>
+                                        <li>-  Former des informaticiens, dotés de solides compétences conceptuelles et technique</li>
+                                        <li>-  Maîtrise de la conception et de la réalisation d'applications exigeantes en terme de sécurité, de performance ou de complexité architecturale</li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div class="caracteristique">
-                <img src="./views/ressources/master.jpg" class="masterImage">
-                <div>
-                    <h1>Caractéristiques</h1>
-                    <ul>
-                        <li>Régimes d'études : Formation initiale / Alternance</li>
-                        <li>Langue : Français</li>
-                        <li>Crédits ECTS : 60 crédits</li>
-                        <li>Stage : 18 semaines / Pas de stage en alternance</li>
-                        <li>Capacité d'accueil : 60</li>
-                        <li>Type de diplôme : Diplôme national de Master</li>
-                    </ul>
-                </div>
-                
-            </div>
-            <div class="embauche">
-                <img src="./views/ressources/embauche.jpg" class="embaucheImage">
-                <div>
-                <h1>Embauche et avantages</h1>
-                    <ul>
-                        <li>Régimes d'études : Formation initiale / Alternance</li>
-                        <li>Langue : Français</li>
-                        <li>Crédits ECTS : 60 crédits</li>
-                        <li>Stage : 18 semaines / Pas de stage en alternance</li>
-                        <li>Capacité d'accueil : 60</li>
-                        <li>Type de diplôme : Diplôme national de Master</li>
-                    </ul>
+        </div>
+        <div class="caracteristique">
+            <div class="container">
+                <div class="row">
+                    <!-- <img src="./views/ressources/master.jpg" class="masterImage"> -->
+                    <div class="col s12">
+                        <h1>Caractéristiques</h1>
+                    </div>
+                    <div class="row">
+                        <ul>
+                            <li class="col s6">Régimes d'études : Formation initiale / Alternance</li>
+                            <li class="col s6">Stage : 18 semaines / Pas de stage en alternance</li>
+                            <li class="col s6">Langue : Français</li>
+                            <li class="col s6">Crédits ECTS : 60 crédits</li>
+                            <li class="col s6">Capacité d'accueil : 60</li>
+                            <li class="col s6">Type de diplôme : Diplôme national de Master</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-		</div>
+        </div>
+        <div class="embauche">
+            <div class="container">
+                <div class="row">
+                    <!-- <img src="./views/ressources/embauche.jpg" class="embaucheImage"> -->
+                    <div class="col s12">
+                        <h1>Embauche et avantages</h1>
+                    </div>
+                    <div class="row">
+                        <p>Cette formation a l'avantage d'avoir un dispositif d'alternance. Ce dispositif permet à l'étudiant de bénéficier d'un statut particulier dû à sa présence en formation mais aussi en entreprise. L'avantage de cette expérience est de pouvoir consolider mais aussi approfondir ses connaissances dans un milieu professionnel. De plus, l'étudiant a l'occasion de se forger un réseau professionnel conséquent qui lui sera utile pour sa carrière.</p>
+                        <p>Grâce à notre diversité dans nos spécialisations en deuxième année de Master, nous arrivons à former nos étudiants pour occuper des postes importants dans les Systèmes d'Informations d'entreprise de domaine divers.</p>
+                        <p>Voici la liste des postes pouvant être occupé par nos étudiants selon leur spécialisation : </p>
+                        <div class="col s6">
+                            <p><u>SITN</u></p>
+                            <ul>
+                                <li class="bulletEmbauche">Chef de projet (MOE, MOA)</li>
+                                <li class="bulletEmbauche">Consultant(e) (MOE, MOA)</li>
+                                <li class="bulletEmbauche">Business Analyste</li>
+                                <li class="bulletEmbauche">Auditeur Systèmes d’Information</li>
+                                <li class="bulletEmbauche">Intégrateur de système</li>
+                                <li class="bulletEmbauche">Architecte des systèmes d’information</li>
+                                <li class="bulletEmbauche">Ingénieur d’étude et développement</li>
+                            </ul>
+                        </div>
+                        <div class="col s6">
+                            <p><u>ID</u></p>
+                            <ul>
+                                <li class="bulletEmbauche">Consultant d´ecisionnel</li>
+                                <li class="bulletEmbauche">Consultant BI</li>
+                                <li class="bulletEmbauche">Data scientist</li>
+                                <li class="bulletEmbauche">Ingénieur d'etudes</li>
+                                <li class="bulletEmbauche">Analyste d´eveloppeur décisionnel</li>
+                                <li class="bulletEmbauche">Expert fonctionnel entrepôt de données </li>
+                            </ul>
+                        </div>
+                        <div class="col s12 offset-s3">
+                            <p><u>IF</u></p>
+                            <ul>
+                                <li class="bulletEmbauche">Responsable de la production informatique et du support applicatif Banque d'Investissement </li>
+                                <li class="bulletEmbauche">Analyste Quantitatif</li>
+                                <li class="bulletEmbauche">Architecte IT</li>
+                                <li class="bulletEmbauche">Responsable Sécurité</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <p>Le salaire moyen à la sortie du Master MIAGE est entre 40k et 45k brut annuel et l'embauche est pratiquement immédiate à la sortie de la formation.</p>
+                </div>
+            </div>
+        </div>
     </body>
+    <footer class="page-footer center">
+        Equipe 7 | FIEVEZ Romain BENDIB Yakine LEDOUX Louis
+    </footer>
 </html>
